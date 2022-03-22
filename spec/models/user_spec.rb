77e97868f83d4e5c09b10be8621cd097def94a4a
kh-amir-tn <amir.khemissi@insat.ucar.tn>
2022-03-22 12:09:04 +0100

    GLv3-3202: 	Backend:              - Set the API endpoint for the signup '/api/v1/users.json'.              - Made some changes to Rubocop.              - Added a request spec for the users API and prepared the tests for the signup with/without valid user data.              - Added a password confirmation examples for the User model.              - Improved routing compatibility between SSR (Server side routing) and CSR (Client side routing).              - Tried defining an approach for error and request handling.              - Removed /ruby-build. 	     - Improved API resiliency by introducing a much stricter API:             	* API calls has to have an explicit `.json` format along with an `Accept: 'application/json'` header.          -> The TODOs were left as comments in the code.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,6 +13,31 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_presence_of(:provider) }
 
     it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:password_confirmation) }
     it { is_expected.to validate_uniqueness_of(:email).scoped_to(:provider).case_insensitive }
+
+    context 'password confirmation' do
+      let(:valid_user) do
+        {
+          name: Faker::Name.name,
+          email: Faker::Internet.email,
+          provider: 'greenlight',
+          password: 'Password123+',
+          password_confirmation: 'Password123+'
+        }
+      end
+
+      it 'validate the record for confirmed password' do
+        user = described_class.new valid_user
+        expect(user).to be_valid
+      end
+
+      it 'invalidate the record for mismatched password confirmation' do
+        user = described_class.new valid_user
+        user.password_confirmation = "#{user.password}extra"
+        expect(user).to be_invalid
+        expect(user.errors.first.details).to eq({ error: :confirmation, attribute: 'Password' })
+      end
+    end
   end
 end
